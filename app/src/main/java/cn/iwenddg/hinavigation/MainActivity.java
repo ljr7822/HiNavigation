df745945f71a5b3fdfcb3870ef00296224c0d54e
iwenLjr <1816807688@qq.com>
2021-06-22 20:46:49 +0800

    添加我们的自定义json导航

--- app/src/main/java/cn/iwenddg/hinavigation/MainActivity.java
+++ app/src/main/java/cn/iwenddg/hinavigation/MainActivity.java
@@ -15,6 +15,7 @@ import cn.iwenddg.hinavigation.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,13 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Use our own NavGraphBuilder
+        //NavGraphBuilder.build(navController,this,);
     }
 
 }
